209f20ed9ff28e4b12876824675613ad28523cd8
Xhr!st!n3 <hassymaya@gmail.com>
2026-05-16 06:54:36 +0000

    feat: add admin stats endpoint with caching (#109)

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common"
+import { AdminModule } from "./admin/admin.module"
 import { GatewaysModule } from "./gateways/gateways.module"
 import { HealthController } from "./health/health.controller"
 import { TagsModule } from "./tags/tags.module"
 
 @Module({
-  imports: [GatewaysModule, TagsModule],
+  imports: [AdminModule, GatewaysModule, TagsModule],
   controllers: [HealthController],
   providers: [],
 })
